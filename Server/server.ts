905fb1f20baf88bf160f59d53f920e0cb8dc3a0c
marbleville <allen.ehrhardt@gmail.com>
2024-05-15 10:08:03 -0400

    added skeleton endpoints to server.ts

--- Server/server.ts
+++ Server/server.ts
@@ -1,6 +1,43 @@
 import express from "express";
 const app = express();
+const PORT: Number = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World");
+app.get("/api/v1/register", (req, res) => {
+	res.sendStatus(401);
+});
+
+app.get("/api/v1/getPublishers", (req, res) => {
+	res.sendStatus(401);
+});
+
+app.post("/api/v1/createSheet", (req, res) => {
+	res.sendStatus(401);
+});
+
+app.post("/api/v1/getSheets", (req, res) => {
+	res.sendStatus(401);
+});
+
+app.post("/api/v1/deleteSheet", (req, res) => {
+	res.sendStatus(401);
+});
+
+app.post("/api/v1/getUpdatesForSubscription", (req, res) => {
+	res.sendStatus(401);
+});
+
+app.post("/api/v1/getUpdatesForPublished", (req, res) => {
+	res.sendStatus(401);
+});
+
+app.post("/api/v1/updatePublished", (req, res) => {
+	res.sendStatus(401);
+});
+
+app.post("/api/v1/updateSubscription", (req, res) => {
+	res.sendStatus(401);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
