d292dd4dc4b1c8aac5301f944fa723713cde4223
Kristoffer Amerman <kristofferamerman@kris-mac.local>
2024-05-21 16:35:33 -0400

    basic fetching

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,13 +1,34 @@
+import { useEffect } from "react"
+import { fetchWithAuth } from "../utils";
 
 
 /**
- * The sheet dashboard page. /user/sheetname
  * @author kris-amerman
+ * @description The dashboard page. Displays all sheets for the current user. Provides options to create, edit, and delete sheets.
  */
 function Dashboard() {
+
+  const fetchData = async () => {
+    try {
+      const response = await fetchWithAuth('http://localhost:3000/api/v1/getSheets');
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      } else {
+        console.error('Failed to fetch');
+      }
+    } catch (error) {
+      console.error('Error fetching', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div>
-      Dashboard
+      <h1>Dashboard</h1>
     </div>
   )
 }
