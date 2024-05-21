57ed6334eea80087941841d322902853507a112b
marbleville <allen.ehrhardt@gmail.com>
2024-05-21 15:29:16 -0400

    cheged to .ts file

---
+++ server/src/database/db.ts
@@ -0,0 +1,23 @@
+import { RowDataPacket } from "mysql2";
+
+/**
+ * Extends the RowDataPacket from the mysql2 library to have the correct typing for the getSheets query.
+ *
+ * @author hunterbrodie
+ */
+interface GetSheetRow extends RowDataPacket {
+	sheetid: number;
+	sheetname: string;
+	latest?: string;
+}
+
+/**
+ * Extends the RowDataPacket from the mysql2 library to have the correct typing for any query that gets a list of users.
+ *
+ * @author hunterbrodie
+ */
+interface GetUserRow extends RowDataPacket {
+	username: string;
+}
+
+export { GetSheetRow, GetUserRow };
