657e188f027392e8de9ce465ce9a4ee92ec73415
Hunter Brodie <hunterbrodie@gmail.com>
2024-05-21 14:51:23 -0400

    added ocmments

--- server/src/db.tsx
+++ server/src/db.tsx
@@ -1,15 +1,22 @@
 import { RowDataPacket } from "mysql2"
 
+
+/**
+ * Extends the RowDataPacket from the mysql2 library to have the correct typing for the getSheets query.
+ *
+ * @author hunterbrodie
+ */
 interface GetSheetRow extends RowDataPacket {
   sheetid: number;
   sheetname: string;
   latest?: string;
 }
 
-interface GetPublisherRow extends RowDataPacket {
-  username: string;
-}
-
-interface GetSubscriberRow extends RowDataPacket {
+/**
+ * Extends the RowDataPacket from the mysql2 library to have the correct typing for any query that gets a list of users.
+ *
+ * @author hunterbrodie
+ */
+interface GetUserRow extends RowDataPacket {
   username: string;
 }
