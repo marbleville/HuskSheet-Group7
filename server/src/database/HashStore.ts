514e39c60ed713770d12710b147ff7a642108c6d
marbleville <allen.ehrhardt@gmail.com>
2024-06-03 16:21:43 -0400

    added docs for hashstore

--- server/src/database/HashStore.ts
+++ server/src/database/HashStore.ts
@@ -3,11 +3,22 @@ import DatabaseInstance from "../database/databaseInstance";
 import DatabaseQueries from "../../../types/queries";
 import { GetAllUpdates, GetSheetID } from "../database/db";
 
+/**
+ * Singleton class that stores a cache of each of the sheets in the database.
+ *
+ * @author marbleville, huntebrodie
+ */
 export default class HashStore {
 	private static sheets: Array<Map<Ref, Term>>;
 
 	constructor() {}
 
+	/**
+	 * Initializes the HashStore with the current state of the database.
+	 * Ensures the HashStore is a singleton instance.
+	 *
+	 * @author marbleville, huntebrodie
+	 */
 	public static async initHash(): Promise<void> {
 		if (HashStore.sheets != null) {
 			return;
@@ -36,6 +47,16 @@ export default class HashStore {
 		});
 	}
 
+	/**
+	 *
+	 * @param publisher the publisher of the sheet ot get the payload for
+	 * @param sheetName the name of the sheet to get the payload for
+	 *
+	 * @returns a newline delimited string represting the value of each cell
+	 * 			in the sheet
+	 *
+	 * @author marbleville, huntebrodie
+	 */
 	public static async getSheetPayload(
 		publisher: string,
 		sheetName: string
@@ -53,6 +74,15 @@ export default class HashStore {
 		return payload;
 	}
 
+	/**
+	 * Updates teh cache of the given sheet with the new changes in the payload.
+	 *
+	 * @param sheetName the name of the sheet to update
+	 * @param publisher the publisher of the sheet to update
+	 * @param payload the payload coantinig new changes to add to the cache
+	 *
+	 * @author marbleville, huntebrodie
+	 */
 	public static async updateSheetPayload(
 		sheetName: string,
 		publisher: string,
@@ -73,6 +103,16 @@ export default class HashStore {
 		}
 	}
 
+	/**
+	 * Returns the ID of the sheet with the given name and publisher.
+	 *
+	 * @param publisher the publisher of the sheet to get the ID for
+	 * @param sheetName the name of the sheet to get the ID for
+	 *
+	 * @returns the ID of the sheet with the given name and publisher
+	 *
+	 * @author marbleville, huntebrodie
+	 */
 	private static async getSheetID(
 		publisher: string,
 		sheetName: string
@@ -84,6 +124,16 @@ export default class HashStore {
 		return sheetIDArr[0].sheetid;
 	}
 
+	/**
+	 * Returns a Ref object representing the reference in the given string.
+	 *
+	 * @param ref a string representing a reference to a cell in the form
+	 * 			  $[col][row]
+	 *
+	 * @returns a Ref object representing the reference
+	 *
+	 * @author marbleville, huntebrodie
+	 */
 	private static getRefFromString(ref: string): Ref {
 		let refWiothout$ = ref.substring(1);
 
