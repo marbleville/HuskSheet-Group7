c48805e9717b42f5ec43beda1e1e005aa46278d1
rishavsarma5 <rishavsarma5@gmail.com>
2024-06-06 13:26:17 -0400

    added delete row/column and payload works

--- client/src/pages/Sheet.tsx
+++ client/src/pages/Sheet.tsx
@@ -78,7 +78,12 @@ const Sheet: React.FC = () => {
 
   const [numRows, setNumRows] = useState(INITIALSHEETROWSIZE);
   const [numCols, setNumCols] = useState(INITIALSHEETCOLUMNSIZE);
-  const [newlyCreatedCells, setNewlyCreatedCells] = useState<Set<string>>(new Set());
+  const [newlyAddedCells, setNewlyAddedCells] = useState<Set<string>>(
+    new Set()
+  );
+  const [newlyDeletedCells, setNewlyDeletedCells] = useState<Set<string>>(
+    new Set()
+  );
 
   /**
    * @description Updates the sheetData when a cell's input changes.
@@ -87,6 +92,9 @@ const Sheet: React.FC = () => {
    */
   const handleCellUpdate = (value: string, cellId: string) => {
     console.log(`should be called for ${cellId} with value: ${value}`);
+    if (value === "<DELETED>" || value === "<CREATED>") {
+      value = "";
+    }
     setSheetData((prevSheetData) => {
       const updatedSheetData = { ...prevSheetData, [cellId]: value };
       prevCellDataRef.current = {
@@ -106,12 +114,22 @@ const Sheet: React.FC = () => {
     // iterates through sheetData and stores updates in a new-line delimited string
     const getAllCellUpdates = (): string => {
       const payload: string[] = [];
+
+      for (const ref of newlyDeletedCells) {
+        payload.push(`${ref} <DELETED>`);
+      }
+
+      for (const ref of newlyAddedCells) {
+        payload.push(`${ref} <CREATED>`);
+      }
+
       for (const [ref, valueAtCell] of Object.entries(sheetData)) {
         const prevValueAtCell = prevCellDataRef.current[ref] || "";
-        if (valueAtCell !== prevValueAtCell || newlyCreatedCells.has(ref)) {
+        if (valueAtCell !== prevValueAtCell) {
           payload.push(`${ref} ${valueAtCell}`);
         }
       }
+
       return payload.join("\n");
     };
 
@@ -133,6 +151,10 @@ const Sheet: React.FC = () => {
         method: "POST",
         body: JSON.stringify(allUpdates),
       });
+
+      // Reset newlyAddedCells and newlyDeletedCells sets to empty after successful fetch
+      setNewlyAddedCells(new Set());
+      setNewlyDeletedCells(new Set());
     } catch (error) {
       console.error("Error publishing new changes", error);
     }
@@ -143,36 +165,105 @@ const Sheet: React.FC = () => {
 
     const newSheetData = { ...sheetData };
     const newRowNumber = numRows + 1;
-    const newCells = new Set(newlyCreatedCells);
+    const newCells = new Set(newlyAddedCells);
 
-    for (let col = 0; col <= numCols; col++) {
+    for (let col = 0; col < numCols; col++) {
       const colLetter = getHeaderLetter(col);
       const cellID = `$${colLetter}${newRowNumber}`;
-      newSheetData[cellID] = ""
+      newSheetData[cellID] = "";
       prevCellDataRef.current[cellID] = "";
       newCells.add(cellID);
+
+      // Check if the newly added cell was marked as deleted before
+      if (newlyDeletedCells.has(cellID)) {
+        newlyDeletedCells.delete(cellID); // Remove the cell from deletedCells
+        newCells.delete(cellID);
+      }
     }
 
     setSheetData(newSheetData);
-    setNewlyCreatedCells(newCells);
+    setNewlyAddedCells(newCells);
+  };
+
+  const deleteRow = () => {
+    if (numRows === 1) {
+      return;
+    }
+
+    setNumRows((prevNumRows) => prevNumRows - 1);
+    const lastRowNumber = numRows;
+
+    const newSheetData = { ...sheetData };
+    const deletedCells = new Set(newlyDeletedCells);
+
+    for (let col = 0; col < numCols; col++) {
+      const colLetter = getHeaderLetter(col);
+      const cellID = `$${colLetter}${lastRowNumber}`;
+      delete newSheetData[cellID];
+      delete prevCellDataRef.current[cellID];
+      deletedCells.add(cellID);
+
+      // Check if the newly deleted cell was added before
+      if (newlyAddedCells.has(cellID)) {
+        newlyAddedCells.delete(cellID); // Remove the cell from addedCells
+        deletedCells.delete(cellID);
+      }
+    }
+
+    setSheetData(newSheetData);
+    setNewlyDeletedCells(deletedCells);
   };
 
   const addNewCol = () => {
     setNumCols((prevNumCols) => prevNumCols + 1);
 
     const newSheetData = { ...sheetData };
-    const newColumnLetter = getHeaderLetter(numCols + 1);
-    const newCells = new Set(newlyCreatedCells);
+    const newColumnLetter = getHeaderLetter(numCols);
+    const newCells = new Set(newlyAddedCells);
 
     for (let row = 1; row <= numRows; row++) {
       const cellID = `$${newColumnLetter}${row}`;
-      newSheetData[cellID] = ""
+      newSheetData[cellID] = "";
       prevCellDataRef.current[cellID] = "";
       newCells.add(cellID);
+
+      // Check if the newly added cell was marked as deleted before
+      if (newlyDeletedCells.has(cellID)) {
+        newlyDeletedCells.delete(cellID); // Remove the cell from deletedCells
+        newCells.delete(cellID);
+      }
     }
 
     setSheetData(newSheetData);
-    setNewlyCreatedCells(newCells);
+    setNewlyAddedCells(newCells);
+  };
+
+  const deleteCol = () => {
+    if (numCols === 1) {
+      return;
+    }
+
+    setNumCols((prevNumCols) => prevNumCols - 1);
+
+    const newSheetData = { ...sheetData };
+    const lastColumnLetter = getHeaderLetter(numCols - 1);
+    const deletedCells = new Set(newlyDeletedCells);
+
+    for (let row = 1; row <= numRows; row++) {
+      const cellID = `$${lastColumnLetter}${row}`;
+      delete newSheetData[cellID];
+      delete prevCellDataRef.current[cellID];
+      deletedCells.add(cellID);
+
+      // Check if the newly deleted cell was added before
+      if (newlyAddedCells.has(cellID)) {
+        newlyAddedCells.delete(cellID); // Remove the cell from addedCells
+        deletedCells.delete(cellID);
+      }
+    }
+
+    setSheetData(newSheetData);
+    setNewlyDeletedCells(deletedCells);
   };
 
   /**
@@ -181,8 +272,6 @@ const Sheet: React.FC = () => {
    * @author rishavsarma5, eduardo-ruiz-garay
    * @returns the new headers for the columns
    */
-
-  // TODO: make dynamic
   const renderSheetHeader = () => {
     const headers = [];
     headers.push(<th key="header-empty" className="header"></th>);
@@ -198,17 +287,29 @@ const Sheet: React.FC = () => {
     }
 
     headers.push(
-      <th key="add-column-header">
+      <th key="delete-column-header" className="button-header">
+        <button
+          onClick={deleteCol}
+          className="delete-column-button"
+          key="delete-column-button"
+        >
+          - Column
+        </button>
+      </th>
+    );
+
+    headers.push(
+      <th key="add-column-header" className="button-header">
         <button
           onClick={addNewCol}
           className="add-column-button"
           key="add-column-button"
         >
-          Add Column
+          + Column
         </button>
       </th>
     );
-  
+
     return <tr>{headers}</tr>;
   };
 
@@ -218,8 +319,6 @@ const Sheet: React.FC = () => {
    * @author rishavsarma5, eduardo-ruiz-garay
    * @returns the row headers and cells per row
    */
-
-  // TODO: make dynamic
   const renderSheetRows = () => {
     const rows = [];
 
@@ -250,22 +349,29 @@ const Sheet: React.FC = () => {
       );
     }
 
-    return (
-      <>
-        {rows}
-        <tr>
-          <td colSpan={numCols + 1}>
-            <button
-              onClick={addNewRow}
-              className="add-row-button"
-              key="add-row-button"
-            >
-              Add Row
-            </button>
-          </td>
-        </tr>
-      </>
+    rows.push(
+      <tr key="row-buttons">
+        <td className="button-header">
+          <button
+            onClick={deleteRow}
+            className="delete-row-button"
+            key="delete-row-button"
+          >
+            - Row
+          </button>
+          <button
+            onClick={addNewRow}
+            className="add-row-button"
+            key="add-row-button"
+          >
+            + Row
+          </button>
+        </td>
+        <td colSpan={numCols}></td>
+      </tr>
     );
+
+    return <>{rows}</>;
   };
 
   // html for rendering sheet
