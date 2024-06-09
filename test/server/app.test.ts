5cd0c23a5e50c367b401af2638f4be57c0ddd526
marbleville <allen.ehrhardt@gmail.com>
2024-06-09 13:19:51 -0400

    finsihed testing register

--- test/server/app.test.ts
+++ test/server/app.test.ts
@@ -2,7 +2,10 @@ const request = require("supertest");
 import app from "../../server/src/app";
 import { Response } from "supertest";
 import { assembleResultObject } from "../../server/src/utils";
-import { assembleTestArgumentObject, setupDB } from "../utils";
+import { setupDB } from "../utils";
+import DatabaseInstance from "../../server/src/database/databaseInstance";
+import DatabaseQueries from "../../types/queries";
+import { GetUserRow } from "../../server/src/database/db";
 
 describe("Tests auth checks in server.ts", () => {
 	it("register should fail if the user password is wrong", async () => {
@@ -22,6 +25,26 @@ describe("Tests auth checks in server.ts", () => {
 			.get("/api/v1/register")
 			.auth("caroline", "123");
 
-		expect(response.body).toEqual(assembleResultObject(true, null, []));
+		expect(response.statusCode).toBe(200);
+		expect(JSON.parse(response.text)).toEqual(
+			assembleResultObject(true, null, [])
+		);
+	});
+
+	it("register should add a new user", async () => {
+		await setupDB();
+
+		const response: Response = await request(app)
+			.get("/api/v1/register")
+			.auth("caroline-but-wrong", "123");
+
+		expect(response.statusCode).toBe(200);
+		expect(JSON.parse(response.text).success).toBeFalsy();
+
+		let user = await DatabaseInstance.getInstance().query<GetUserRow>(
+			DatabaseQueries.getUser("caroline-but-wrong")
+		);
+
+		expect(user.length).toBe(1);
 	});
 });
