7342869bc256fc9bc65630eb87889d40e26ddf47
Hunter Brodie <hunterbrodie@gmail.com>
2024-06-10 13:47:42 -0400

    Coverage!

--- db-scripts/store-proc.sh
+++ db-scripts/store-proc.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+cd /app
+
 ############################
 # Store procedure on MySQL #
 ############################
-mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DB < /reset-proc.sql
+mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DB < ./db-scripts/reset-proc.sql
 
 #######################
 # Run Jest Test Suite #
 #######################
-npm install --prefix /server
-npm install --prefix /test && npm run test --prefix /test
+npm install --prefix ./server
+npm install && npm run test
