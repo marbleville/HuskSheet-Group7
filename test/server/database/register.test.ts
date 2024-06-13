82abe3d658f06ad041f0807d71f58421e0015134
marbleville <allen.ehrhardt@gmail.com>
2024-06-13 16:07:59 -0400

    added stress test

--- test/server/database/register.test.ts
+++ test/server/database/register.test.ts
@@ -1,7 +1,8 @@
-import { setupDB } from "../../utils";
+import { assembleTestArgumentObject, setupDB } from "../../utils";
 import { register } from "../../../server/src/functions/register";
 import { getPublishers } from "../../../server/src/functions/getPublishers";
 import { Argument } from "../../../types/types";
+import { assembleResultObject } from "../../../server/src/utils";
 
 describe("register", () => {
 	it("checks to see if the isPublished field is updated", async () => {
